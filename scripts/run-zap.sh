73e2b830909405cb7a55375c3f86a6a22788e0b3
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-21 01:15:38 +0530

    Update run-zap.sh

--- scripts/run-zap.sh
+++ scripts/run-zap.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 set -euxo pipefail
+
+mkdir -p /zap/wrk/reports
+
+echo "ZAP Version"
+zap.sh -version
+
+echo "Automation File"
+cat /zap/wrk/doc-enrich-zap-dast/zap/automation.yaml
+
+zap.sh -cmd -autorun /zap/wrk/doc-enrich-zap-dast/zap/automation.yaml
+#!/bin/bash
+
+set -euxo pipefail
 mkdir -p /zap/wrk/reports
 echo "ZAP Version"
 zap.sh -version
