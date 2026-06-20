70d8e048c9082f562d4209835de98018ed4e0b39
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-21 00:33:11 +0530

    Update w3id_auth.js

--- zap/w3id_auth.js
+++ zap/w3id_auth.js
@@ -1,1 +1,30 @@
+function authenticate(helper, paramsValues, credentials) {
 
+    var By = org.openqa.selenium.By;
+
+    helper.openUrl(paramsValues.get("LoginURL"));
+
+    var wd = helper.getWebDriver();
+
+    java.lang.Thread.sleep(5000);
+
+    wd.findElement(
+        By.id("user-name-input")
+    ).sendKeys(
+        credentials.getParam("username")
+    );
+
+    wd.findElement(
+        By.id("password-input")
+    ).sendKeys(
+        credentials.getParam("password")
+    );
+
+    wd.findElement(
+        By.id("login-button")
+    ).click();
+
+    java.lang.Thread.sleep(15000);
+
+    return true;
+}
