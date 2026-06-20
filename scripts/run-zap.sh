5cf61dd91b66d611edf0c4c41d5fd95984cfc0ff
devopscicd17 <86373586+devopscicd17@users.noreply.github.com>
2026-06-21 00:58:12 +0530

    Update run-zap.sh

--- scripts/run-zap.sh
+++ scripts/run-zap.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+echo "Current Directory:"
+pwd
+
+echo "Repository Contents:"
+find /zap/wrk/doc-enrich-zap-dast
+
+echo "Automation File:"
+ls -l /zap/wrk/doc-enrich-zap-dast/zap/automation.yaml
+
+zap.sh -cmd 
+-autorun /zap/wrk/doc-enrich-zap-dast/zap/automation.yaml
+#!/bin/bash
+
 set -euo pipefail
 
 echo "Starting OWASP ZAP Automation Framework"
